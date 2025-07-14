698e1afbbfd52de82551eff61494b47a08a29a89
Sameer Bhat <sameerbhat76@gmail.com>
2025-07-14 09:46:31 +0100

    feat: add GTE multilingual base embedding model to the factory

--- oncopro/src/embeddings/factory.py
+++ oncopro/src/embeddings/factory.py
@@ -10,13 +10,14 @@ from .qwen34B import Qwen34BEmbedding
 from .nvembedv2 import NVEmbedV2
 from .bgem3 import BGEM3Embedding
 from .mpnetbase2 import MPNetBase2Embedding
+from .gte import GTEMultilingualBaseEmbedding
 
 
 class EmbeddingModelFactory:
     """Factory class for creating embedding models."""
     
     # Auto-discover models from imported classes
-    _model_classes = [Jina4Embedding, Qwen34BEmbedding, OpenAIEmbedding, NVEmbedV2, BGEM3Embedding, MPNetBase2Embedding]
+    _model_classes = [Jina4Embedding, Qwen34BEmbedding, OpenAIEmbedding, NVEmbedV2, BGEM3Embedding, MPNetBase2Embedding, GTEMultilingualBaseEmbedding]
     
     @classmethod
     def _get_models_registry(cls) -> Dict[str, Type[EmbeddingModel]]:
