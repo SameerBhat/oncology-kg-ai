938c93d1d5eb7ef4405e3d2aae1171f70c962d11
Sameer Bhat <sameerbhat76@gmail.com>
2025-07-14 09:41:29 +0100

    feat: add MPNetBase2 embedding model to the factory for multilingual support

--- oncopro/src/embeddings/factory.py
+++ oncopro/src/embeddings/factory.py
@@ -9,13 +9,14 @@ from .openai import OpenAIEmbedding
 from .qwen34B import Qwen34BEmbedding
 from .nvembedv2 import NVEmbedV2
 from .bgem3 import BGEM3Embedding
+from .mpnetbase2 import MPNetBase2Embedding
 
 
 class EmbeddingModelFactory:
     """Factory class for creating embedding models."""
     
     # Auto-discover models from imported classes
-    _model_classes = [Jina4Embedding, Qwen34BEmbedding, OpenAIEmbedding, NVEmbedV2, BGEM3Embedding]
+    _model_classes = [Jina4Embedding, Qwen34BEmbedding, OpenAIEmbedding, NVEmbedV2, BGEM3Embedding, MPNetBase2Embedding]
     
     @classmethod
     def _get_models_registry(cls) -> Dict[str, Type[EmbeddingModel]]:
